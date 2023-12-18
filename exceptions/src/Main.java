b8912d8f4b43ab10a72317724f2d23cacf8899d0
GuiMedeirox <omaewa9111@gmail.com>
2023-12-18 19:01:31 -0300

    .

--- exceptions/src/Main.java
+++ exceptions/src/Main.java
@@ -1,8 +1,36 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Check the branches");
+        Scanner sc = null;
 
+        System.out.println("MAIN");
+        m1();
+        try{
+            m1();
+//            sc.next();
+        }catch (ArithmeticException | NullPointerException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+
+        }
+        System.out.println("MAINF");
+
+    }
+
+    static void m1(){
+        System.out.println("m1");
+        m2();
+        System.out.println("Fm1");
+    }
+
+    static  void m2(){
+        System.out.println("M2");
+        int a = 0;
+        for (int i = 0; i < 5; i++) {
+            a = a / 0;
+            System.out.println(a);
+        }
+        System.out.println("FM2");
     }
 
 }
