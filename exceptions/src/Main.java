e499626527c2aae34a065761cbc324287c376019
GuiMedeirox <omaewa9111@gmail.com>
2023-12-19 16:06:00 -0300

    checked-unchecked-Exceptions

--- exceptions/src/Main.java
+++ exceptions/src/Main.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
+import java.util.zip.DataFormatException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("MAIN");
         try {
             m1();
-        } catch(ArithmeticException ex){
+        } catch(ArithmeticException | OpenFile ex){
             System.out.println(ex.getMessage());
         }
         System.out.println("MAINF");
 
     }
 
-    static void m1(){
+    static void m1() throws OpenFile {
         System.out.println("m1");
-        m2();
+        //sete o valor para A
+        m2(3);
         System.out.println("Fm1");
     }
 
-    static  void m2(){
+    static  void m2(int a) throws OpenFile{
         System.out.println("M2");
-        throw new ArithmeticException("Erro de ArithmeticException Customizado");
+        if(a == 2){
+            System.out.println("Opa, ta tudo bacana");
+        }else {
+            throw new OpenFile("Unknown File");
+        }
+        System.out.println("FM2");
     }
-
 }
---
+++ exceptions/src/OpenFile.java
@@ -0,0 +1,7 @@
+public class OpenFile extends Exception {
+
+    public OpenFile(String msg){
+        super(msg);
+    }
+
+}
