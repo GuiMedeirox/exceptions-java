8798d8f819639d72fe8c8a456a168f9b84130cb9
GuiMedeirox <omaewa9111@gmail.com>
2023-12-20 14:46:48 -0300

    try-with-resources

--- exceptions/src/Main.java
+++ exceptions/src/Main.java
@@ -3,11 +3,12 @@ import java.util.zip.DataFormatException;
 
 public class Main {
     public static void main(String[] args) {
-        Account a = new Account();
-        try {
-            a.toDeposit(-10);
-        } catch (NegativeDepositException ex){
+
+        try(Conexao con = new Conexao()){
+            con.leDados();
+        } catch(IllegalStateException ex){
             System.out.println(ex.getMessage());
         }
+
     }
 }
